16391fbc8be59fc5b719a2ca23c82ddc2a8764f5
julietcass71 <doctoracassano@gmail.com>
2025-10-18 03:18:57 +0000

    Scaffold Cookiecutter + dataset (Entrega 2)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Descripción del Dataset y Origen',
+    author='Nancy Julieta Cassano',
+    license='MIT',
+)
